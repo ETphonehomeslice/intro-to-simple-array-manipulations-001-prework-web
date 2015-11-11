59f268de03c70558a5d43fc2b356778ad6d58c69
ETphonehomeslice <et88guy@hotmail.com>
2015-11-11 01:41:59 -0500

    done the more correct way

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,101 +1,48 @@
-countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-next_country = "Niger"
-
 def using_push(countries_in_western_africa, next_country)
-  countries_in_western_africa.push(next_country)
+  return countries_in_western_africa.push(next_country)
 end
-
-using_push(countries_in_western_africa, next_country)
-
-neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-new_neighborhood = "Brooklyn Heights"
-
 
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-    neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
-end
-
-using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-
-great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+   neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+ end
 
 def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties.pop 
 end
 
-using_pop(great_hits_of_the_nineties)
-
-chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
-
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones.pop(2)
 end
 
-pop_with_args(chars_in_game_of_thrones)
-
-my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-
 def using_shift(my_favorite_cities)
-  my_favorite_cities.shift
+  my_favorite_cities.shift 
 end
-
-ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
 
 def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
 
-shift_with_args(ice_cream_brands)
-
-my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-more_favs = ["mario kart", "flatiron school"]
-
 def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-using_concat(my_favorite_things, more_favs)
-
-list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-another_esoteric_language = "Malbolge"
-
-def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+def using_insert(list_of_esoteric_languages, another_esoteric_language)
+  list_of_esoteric_languages.insert(4, another_esoteric_language)
 end
-
-using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-
-captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-
 
 def using_uniq(captain_planet_and_the_planeteers)
-  captain_planet_and_the_planeteers.uniq
+  captain_planet_and_the_planeteers.uniq 
 end
-
-using_uniq(captain_planet_and_the_planeteers)
-
-
-private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-
 
 def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten 
 end
 
-using_flatten(private_colleges_in_newyork)
-
-instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-
-def using_delete(instructors, i = 2)
-  instructors.delete(i)
+def using_delete(instructors, get_rid_of = "Steven")
+  instructors.delete(get_rid_of)
 end
 
-using_delete(instructors, i = 2)
-
-famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-
-def using_delete_at(famous_robots, i = 2)
-  famous_robots.delete_at(i)
+def using_delete_at(famous_robots, delete_this = 2)
+  famous_robots.delete_at(delete_this)
 end
 
-using_delete_at(famous_robots, i)
